a40e72b99f40d3f4e226263ee2c94524d3aae609
Ionut <76490950+y0nutz2015123@users.noreply.github.com>
2022-03-11 13:07:48 +0200

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -5,12 +5,12 @@ import "firebase/compat/auth";
 // import { seedDatabase } from "../seed";
 
 const config = {
-  apiKey: "AIzaSyAXf4OifNJDmp6ZgoQ7Dxu5nM9lXlNKbMs",
-  authDomain: "instaclone-abd5b.firebaseapp.com",
-  projectId: "instaclone-abd5b",
-  storageBucket: "instaclone-abd5b.appspot.com",
-  messagingSenderId: "275501253765",
-  appId: "1:275501253765:web:e15b7b25e5e13e790d15ec"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const firebase = Firebase.initializeApp(config);
